7e7408998757a6d242b4712e5ed1f28b76d63e4e
Ashiqur Rahman <ashiquejava@gmail.com>
2020-04-07 10:55:08 +0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# beginner-reactjs
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Clock from "./Clock";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Clock />
+    </div>
+  );
+}
